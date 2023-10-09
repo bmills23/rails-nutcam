2e4a3b299114531e3339009d08551c1c73b6e7c2
Bryan Mills <millsbryang@gmail.com>
2023-10-09 07:31:00 -0600

    Added websocket functionality

--- nutcam/app/controllers/stream_controller.rb
+++ nutcam/app/controllers/stream_controller.rb
@@ -1,32 +1,33 @@
 class StreamController < ApplicationController
   include ActionController::Live
 
-  def live
-    response.headers['Content-Type'] = 'text/event-stream'
-    response.headers['Cache-Control'] = 'no-cache'
-    response.headers['Connection'] = 'keep-alive'
+  # def live
+  #   response.headers['Content-Type'] = 'text/event-stream'
+  #   response.headers['Cache-Control'] = 'no-cache'
+  #   response.headers['Connection'] = 'keep-alive'
 
-    sse = SSE.new(response.stream, retry: 300, event: 'video_stream')
+  #   sse = SSE.new(response.stream, retry: 300, event: 'video_stream')
 
-    video_path = 'path_to_your_video.mp4' # Replace with the actual path to your video file
+  #   video_path = '#' # Replace with the actual path to your video file
 
-    begin
-      File.open(video_path, 'rb') do |file|
-        while (data = file.read(1024)) # Read video data in chunks
-          sse.write(data, event: 'video_frame')
-        end
-      end
-    rescue ActionController::Live::ClientDisconnected
-      logger.info 'Client Disconnected'
-      sse.close
-      response.stream.close
-    rescue IOError
-      logger.info 'IOError'
-      sse.close
-      response.stream.close
-    ensure
-      sse.close
-      response.stream.close
-    end
-  end
+  #   begin
+  #     File.open(video_path, 'rb') do |file|
+  #       while (data = file.read(1024)) # Read video data in chunks
+  #         sse.write(data, event: 'video_frame')
+  #       end
+  #     end
+  #   rescue ActionController::Live::ClientDisconnected
+  #     logger.info 'Client Disconnected'
+  #     sse.close
+  #     response.stream.close
+  #   rescue IOError
+  #     logger.info 'IOError'
+  #     sse.close
+  #     response.stream.close
+  #   ensure
+  #     sse.close
+  #     response.stream.close
+  #   end
+  # end
+
 end
--- nutcam/config/routes.rb
+++ nutcam/config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  get 'live', to: 'stream#live'
-
   root 'welcome#index'
 
   get 'welcome/index'
+
+  get 'live', to: 'stream#live'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
